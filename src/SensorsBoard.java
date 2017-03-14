ac542194a89d47bec1c9289072c987ab96f72e39
Josep de Cid <josep.de.cid@gmail.com>
2017-03-14 19:18:50 +0100

    Refactor adding classes, state change operators and new data structures

--- src/SensorsBoard.java
+++ src/SensorsBoard.java
@@ -3,118 +3,292 @@ import IA.Red.CentrosDatos;
 import IA.Red.Sensor;
 import IA.Red.Sensores;
 
-import java.io.BufferedOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Representation of the board with sensors and data centers information.
+ */
 public class SensorsBoard {
 
-    private int totalCost;
-    private double totalInformation;
+    private Double totalCost;
+    private Double totalInformation;
+
+    private static final Integer MAX_SENSOR_CONNECTIONS = 3;
+    private static final Integer MAX_DATA_CENTER_CONNECTIONS = 25;
 
     private static List<Sensor> sensorList;
-    private static List<Centro> centroList;
+    private static List<Centro> centerList;
 
-    private int[] connections;
+    private List<SensorConnections> sensorConnections;
 
-    public Integer getTotalCost() {
-        return totalCost;
-    }
-
-    public Double getTotalInformation() {
-        return totalInformation;
-    }
-
-    public SensorsBoard() {
+    /**
+     * Default constructor
+     */
+    SensorsBoard() {
         sensorList = new ArrayList<>();
-        centroList = new ArrayList<>();
+        centerList = new ArrayList<>();
 
         generateBoard();
-        connections = new int[sensorList.size()];
+        sensorConnections = new ArrayList<>(sensorList.size());
 
-        totalCost = 0;
+        totalCost = 0D;
         totalInformation = 0D;
 
-        generateInitialState();
+        generateEmptyInitialState();
     }
 
-    public SensorsBoard(SensorsBoard board) {
-        connections = new int[board.getProblemSize()];
-        System.arraycopy(board.connections, 0, connections, 0, board.connections.length);
+    /**
+     * Child state constructor
+     *
+     * @param board Parent state board.
+     */
+    SensorsBoard(SensorsBoard board) {
+        sensorConnections = new ArrayList<>(board.sensorConnections);
         totalCost = board.getTotalCost();
         totalInformation = board.getTotalInformation();
     }
 
+    /**
+     * Generates random board with N sensors and N data centers given a seed.
+     */
     private void generateBoard() {
-
         // TODO: Random
-        sensorList.addAll(new Sensores(100, 123));
-
+        sensorList.addAll(new Sensores(20, 123));
         // TODO: Random
-        centroList.addAll(new CentrosDatos(4, 123));
-    }
-
-    private int calculateCost(Sensor sensor1, Sensor sensor2) {
-        return (sensor1.getCoordX() - sensor2.getCoordX()) * (sensor1.getCoordX() - sensor2.getCoordX())
-                + (sensor1.getCoordY() - sensor2.getCoordY()) * (sensor1.getCoordY() - sensor2.getCoordY());
-    }
-
-    private int calculateInformation(Sensor sensor1, Sensor sensor2) {
-        return 1;
+        centerList.addAll(new CentrosDatos(1, 123));
     }
 
     /*---- INITIAL STATES ----*/
 
-    private void generateInitialState() {
+    /**
+     * Initial state generation without connections.
+     */
+    private void generateEmptyInitialState() {
+        for (int i = 0; i < sensorList.size(); i++) {
+            List<Integer> inputs = new ArrayList<>();
+            sensorConnections.add(new SensorConnections(null, inputs));
+        }
+    }
+
+    /**
+     * Intial state generation with random sequential connections.
+     */
+    private void generateDummyInitialState() {
         for (int i = 0; i < sensorList.size() - 1; i++) {
-            connections[i] = i + 1;
+            List<Integer> inputs = new ArrayList<>();
+            if (i > 0) {
+                inputs.add(i - 1);
+            }
+            sensorConnections.add(new SensorConnections(i + 1, inputs));
             totalCost += calculateCost(sensorList.get(i), sensorList.get(i + 1));
             totalInformation += calculateInformation(sensorList.get(i), sensorList.get(i + 1));
         }
-        connections[sensorList.size() - 1] = -1;
+        List<Integer> inputs = new ArrayList<>();
+        inputs.add(sensorList.size() - 2);
+        sensorConnections.add(new SensorConnections(null, inputs));
+        totalCost += calculateCost(sensorList.get(sensorList.size() - 2), sensorList.get(sensorList.size() - 1));
+        totalInformation += calculateInformation(sensorList.get(sensorList.size() - 2), sensorList.get(sensorList.size() - 1));
     }
 
     /*---- OPERATORS ----*/
 
-    public void swapConnection(int index) {
-        int aux = connections[index];
+    /**
+     * Adds a connection between two sensors.
+     *
+     * @param first  Sensor from that will be an output connection.
+     * @param second Sensor that will receive the input connection.
+     * @return If connection has been added.
+     */
+    boolean addConnection(int first, int second) {
+        if (isAllowedConnection(first, second)) {
+            sensorConnections.get(second).inputSensors.add(first);
+            sensorConnections.get(first).outputSensor = second;
 
-        if (connections[index] >= 0) {
-            int oldCost = calculateCost(sensorList.get(index), sensorList.get(connections[index]));
+            totalCost += calculateCost(sensorList.get(first), sensorList.get(second));
 
-            connections[index] = connections[(index + 1) % sensorList.size()];
-            connections[(index + 1) % sensorList.size()] = aux;
+            return true;
+        }
+        return false;
+    }
 
-            if (connections[index] >= 0) {
-                int newCost = calculateCost(sensorList.get(index), sensorList.get(connections[index]));
-                totalCost += (newCost - oldCost);
+    /**
+     * Removes a connection between two sensors.
+     *
+     * @param first  Sensor from that we wil will be an output connection.
+     * @param second Sensor that will receive the input connection.
+     * @return If
+     */
+    boolean removeConnection(int first, int second) {
+        for (int i = 0; i < sensorConnections.get(second).inputSensors.size(); ++i) {
+            if (sensorConnections.get(second).inputSensors.get(i) == first) {
+                sensorConnections.get(second).inputSensors.remove(i);
+                sensorConnections.get(first).outputSensor = null;
+                return true;
             }
+        }
+        return false;
+    }
+
+    // TODO: Peta fort
+    public void swapConnection(int index) {
+
+        Double oldCost = calculateCost(sensorList.get(index), sensorList.get(sensorConnections.get(index).outputSensor));
+
+        // Swap outputs
+        int aux = sensorConnections.get(index).outputSensor;
+        sensorConnections.get(index).outputSensor = sensorConnections.get((index + 1) % sensorList.size()).outputSensor;
+        sensorConnections.get((index + 1) % sensorList.size()).outputSensor = aux;
+
+        // Swap inputs
+        List<Integer> aux2 = sensorConnections.get(index).inputSensors;
+        sensorConnections.get(index).inputSensors = sensorConnections.get((index + 1) % sensorList.size()).inputSensors;
+        sensorConnections.get((index + 1) % sensorList.size()).inputSensors = aux2;
+
+        if (sensorConnections.get(index).outputSensor < sensorList.size()) {
+            totalCost += (calculateCost(sensorList.get(index), sensorList.get(sensorConnections.get(index).outputSensor)) - oldCost);
         }
     }
 
     /*---- HEURISTICS ----*/
 
-    public int costHeuristic() {
-        return totalCost;
+    /**
+     * Heuristic based on cost.
+     *
+     * @return Heuristic value.
+     */
+    Double costHeuristic() {
+        return totalCost * -1D;
     }
 
+    /**
+     * Heuristic based on information.
+     *
+     * @return Heuristic value.
+     */
     public double informationHeuristic() {
         return 1 / totalInformation;
     }
 
+    /**
+     * Heuristic based on cost and information.
+     *
+     * @return Heuristic value.
+     */
     public double superHeuristic() {
         return totalCost / totalInformation;
     }
 
-    public boolean isGoal() {
+    /*---- Goal functions ----*/
+
+    /**
+     * Checks if it's a goal solution state.
+     *
+     * @return Is a goal state.
+     */
+    boolean isGoal() {
         return false;
     }
 
-    public int getProblemSize() {
+    /*---- Auxiliary classes and functions ----*/
+
+    /**
+     * Representation of a sensor input and output connections.
+     */
+    private class SensorConnections {
+
+        private Integer outputSensor;
+        private List<Integer> inputSensors;
+
+        SensorConnections(Integer outputSensor, List<Integer> inputSensors) {
+            this.outputSensor = outputSensor;
+            this.inputSensors = inputSensors;
+        }
+
+    }
+
+    /**
+     * Calculates the Euclidean distance between two sensors.
+     *
+     * @param sensor1 First sensor with its coordinates.
+     * @param sensor2 Second sensor with its coordinates.
+     * @return Distance cost.
+     */
+    private Double calculateCost(Sensor sensor1, Sensor sensor2) {
+        return Math.sqrt((sensor1.getCoordX() - sensor2.getCoordX()) * (sensor1.getCoordX() - sensor2.getCoordX())
+                + (sensor1.getCoordY() - sensor2.getCoordY()) * (sensor1.getCoordY() - sensor2.getCoordY()));
+    }
+
+    /**
+     * Calculates information cost between two sensors.
+     *
+     * @param sensor1 First sensor with its transmission capacity.
+     * @param sensor2 Second sensor with its transmission capacity.
+     * @return Information cost.
+     */
+    private int calculateInformation(Sensor sensor1, Sensor sensor2) {
+        return 1;
+    }
+
+    /**
+     * Checks if a a connection between two sensors can be established.
+     *
+     * @param first  Sensor from that will be an output connection.
+     * @param second Sensor that will receive the input connection.
+     * @return If the connection can be performed.
+     */
+    private boolean isAllowedConnection(int first, int second) {
+        // Already 3 incoming connections
+        if (sensorConnections.get(second).inputSensors.size() == MAX_SENSOR_CONNECTIONS) {
+            return false;
+        }
+
+        // Already connected first -> second (redundant)
+        Integer nextSensor = sensorConnections.get(first).outputSensor;
+        while (nextSensor != null) {
+            if (nextSensor == second) {
+                return false;
+            }
+            nextSensor = sensorConnections.get(nextSensor).outputSensor;
+        }
+
+        // Already connected second -> first (cycle)
+        nextSensor = sensorConnections.get(second).outputSensor;
+        while (nextSensor != null) {
+            if (nextSensor == first) {
+                return false;
+            }
+            nextSensor = sensorConnections.get(nextSensor).outputSensor;
+        }
+
+        return true;
+    }
+
+    /**
+     * Gets the problem size.
+     *
+     * @return Number of sensors.
+     */
+    int getProblemSize() {
         return sensorList.size();
     }
 
-    public int[] getConnections() {
-        return connections;
+    /**
+     * Gets problem distance cost.
+     *
+     * @return Cost of the current configuration.
+     */
+    Double getTotalCost() {
+        return totalCost;
     }
+
+    /**
+     * Gets problem information cost.
+     *
+     * @return Information value of the current configuration.
+     */
+    Double getTotalInformation() {
+        return totalInformation;
+    }
+
 }
--- src/SensorsSuccessors.java
+++ src/SensorsSuccessors.java
@@ -7,22 +7,22 @@ import java.util.List;
 public class SensorsSuccessors implements SuccessorFunction {
 
     public List getSuccessors(Object state) {
-        ArrayList<Successor> retval = new ArrayList<>();
+        ArrayList<Successor> childrenStates = new ArrayList<>();
         SensorsBoard board = (SensorsBoard) state;
 
-        int min = Integer.MAX_VALUE;
-
         for (int i = 0; i < board.getProblemSize(); i++) {
-            SensorsBoard auxSensors = new SensorsBoard(board);
-            auxSensors.swapConnection(i);
-            retval.add(new Successor("swap connection " + i, auxSensors));
-            if (auxSensors.getTotalCost() < min) {
-                min = auxSensors.getTotalCost();
+            for (int j = 0; j < board.getProblemSize(); j++) {
+                if (i != j) {
+                    SensorsBoard auxSensors = new SensorsBoard(board);
+                    if (auxSensors.addConnection(i, j)) {
+                        childrenStates.add(new Successor("add connection " + i + " - " + j, auxSensors));
+
+                        System.out.println(auxSensors.getTotalCost());
+                    }
+                }
             }
         }
 
-        System.out.println(min);
-
-        return retval;
+        return childrenStates;
     }
 }
